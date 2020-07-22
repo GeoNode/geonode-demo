c8c6742e88dd6818df1a348e2d68424f7c7c22f4
afabiani <alessio.fabiani@geo-solutions.it>
2020-07-23 00:59:19 +0200

    - BR/CI/CD minor settings updates

--- geonode_master/br/backup.sh
+++ geonode_master/br/backup.sh
@@ -32,7 +32,7 @@ fi
 
 cd /usr/src/geonode_master/ 
 
-./manage.sh backup -f -c $PWD/geonode_master/br/settings_docker.ini --backup-dir /$BKP_FOLDER_NAME/
+./manage.sh backup -i -f -c $PWD/geonode_master/br/settings_docker.ini --backup-dir /$BKP_FOLDER_NAME/
 
 BKP_FILE_LATEST=$(find /$BKP_FOLDER_NAME/*.zip -type f -exec stat -c '%Y %n' {} \; | sort -nr | awk 'NR==1,NR==1 {print $2}')
 BKP_FILE_NAME=$(echo $BKP_FILE_LATEST | tail -n 1 | grep -oP -m 1 "\/$BKP_FOLDER_NAME\/\K.*" | sed 's|.zip||')
