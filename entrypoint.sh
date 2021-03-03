19299a90ddc40954d50d05ed4ab876d35a884b3b
afabiani <alessio.fabiani@gmail.com>
2021-03-03 19:33:18 +0100

    [Security] - Bump to python:3.8.8-buster
     - Move cron and memcached restart to entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 # Exit script in case of error
 set -e
 
+# Start cron && memcached services
+service cron restart
+service memcached restart
 
 echo $"\n\n\n"
 echo "-----------------------------------------------------"
@@ -51,7 +54,7 @@ else
         /usr/local/bin/invoke prepare >> /usr/src/geonode_master/invoke.log
         echo "prepare task done"
 
-        if [ ${IS_FIRST_START} = "true" ] || [ ${IS_FIRST_START} = "True" ] || [ ${FORCE_REINIT} = "true" ]  || [ ${FORCE_REINIT} = "True" ] || [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
+        if [ ${FORCE_REINIT} = "true" ]  || [ ${FORCE_REINIT} = "True" ] || [ ! -e "/mnt/volumes/statics/geonode_init.lock" ]; then
             /usr/local/bin/invoke updategeoip >> /usr/src/geonode_master/invoke.log
             echo "updategeoip task done"
             /usr/local/bin/invoke fixtures >> /usr/src/geonode_master/invoke.log
