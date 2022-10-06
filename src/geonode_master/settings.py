aae2125bc442f6113811dbaadd1a924f86084d64
Giovanni Allegri <giohappy@gmail.com>
2022-10-06 10:53:06 +0200

    activvation of new importer

--- src/geonode_master/settings.py
+++ src/geonode_master/settings.py
@@ -141,3 +141,28 @@ if LDAP_ENABLED and 'geonode_ldap' not in INSTALLED_APPS:
 
 # Add your specific LDAP configuration after this comment:
 # https://docs.geonode.org/en/master/advanced/contrib/#configuration
+INSTALLED_APPS += ('dynamic_models', 'importer', 'importer.handlers',)
+
+CELERY_TASK_QUEUES += (
+    Queue('importer.import_orchestrator', GEONODE_EXCHANGE, routing_key='importer.import_orchestrator'),
+    Queue('importer.import_resource', GEONODE_EXCHANGE, routing_key='importer.import_resource', max_priority=8),
+    Queue('importer.publish_resource', GEONODE_EXCHANGE, routing_key='importer.publish_resource', max_priority=8),
+    Queue('importer.create_geonode_resource', GEONODE_EXCHANGE, routing_key='importer.create_geonode_resource', max_priority=8),
+    Queue('importer.import_with_ogr2ogr', GEONODE_EXCHANGE, routing_key='importer.import_with_ogr2ogr', max_priority=10),
+    Queue('importer.import_next_step', GEONODE_EXCHANGE, routing_key='importer.import_next_step', max_priority=3),
+    Queue('importer.create_dynamic_structure', GEONODE_EXCHANGE, routing_key='importer.create_dynamic_structure', max_priority=10),
+    Queue('importer.copy_geonode_resource', GEONODE_EXCHANGE, routing_key='importer.copy_geonode_resource', max_priority=0),
+    Queue('importer.copy_dynamic_model', GEONODE_EXCHANGE, routing_key='importer.copy_dynamic_model'),
+    Queue('importer.copy_geonode_data_table', GEONODE_EXCHANGE, routing_key='importer.copy_geonode_data_table'),
+)
+
+DATABASE_ROUTERS = ["importer.db_router.DatastoreRouter"]
+
+SIZE_RESTRICTED_FILE_UPLOAD_ELEGIBLE_URL_NAMES += ('importer_upload',)
+
+IMPORTER_HANDLERS = os.getenv('IMPORTER_HANDLERS', [
+    'importer.handlers.gpkg.handler.GPKGFileHandler',
+    'importer.handlers.geojson.handler.GeoJsonFileHandler',
+    'importer.handlers.shapefile.handler.ShapeFileHandler',
+    'importer.handlers.kml.handler.KMLFileHandler'
+])
