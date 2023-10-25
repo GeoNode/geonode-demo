d11ece1c6475251df2d6fcd78319be5f79b81502
G. Allegri <giohappy@gmail.com>
2023-10-25 14:48:49 +0200

    Upgrade project

--- src/geonode_demo/wsgi.py
+++ src/geonode_demo/wsgi.py
@@ -41,6 +41,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "geonode_demo.settings")
 # file. This includes Django's development server, if the WSGI_APPLICATION
 # setting points here.
 from django.core.wsgi import get_wsgi_application
+
 application = get_wsgi_application()
 
 # Apply WSGI middleware here.
--- src/setup.py
+++ src/setup.py
@@ -27,24 +27,23 @@ from setuptools import find_packages
 def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
+
 setup(
     name="geonode_demo",
     version="4.0.0",
     author="",
     author_email="",
     description="geonode_demo, based on GeoNode",
-    long_description=(read('README.md')),
+    long_description=(read("README.md")),
     # Full list of classifiers can be found at:
     # http://pypi.python.org/pypi?%3Aaction=list_classifiers
     classifiers=[
-        'Development Status :: 1 - Planning',
+        "Development Status :: 1 - Planning",
     ],
     license="GPL",
     keywords="geonode_demo geonode django",
-    url='https://github.com/geonode_demo/geonode_demo',
+    url="https://github.com/geonode_demo/geonode_demo",
     packages=find_packages(),
-    dependency_links=[
-        "git+https://github.com/GeoNode/geonode.git@3.3.x#egg=geonode"
-    ],
+    dependency_links=["git+https://github.com/GeoNode/geonode.git#egg=geonode"],
     include_package_data=True,
 )
